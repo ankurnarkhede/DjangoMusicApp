5557a67d846f2a4d74b748e5e34b582bd27fb13c
smartankur4u <ankurnarkhede@gmail.com>
2018-01-12 09:24:02 +0530

    22] fav added

--- music/urls.py
+++ music/urls.py
@@ -12,4 +12,14 @@ urlpatterns = [
     # /music/<album.id>...for album id
     url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
 
+    # /music/<album.id>/favorite
+    url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
+
+
 ]
+
+
+
+
+
+
