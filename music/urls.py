a1097e1f0a728629e2070b1713e2373cbb82e1ea
smartankur4u <ankurnarkhede@gmail.com>
2018-01-13 17:43:35 +0530

    32] modified ui...crud for albums

--- music/urls.py
+++ music/urls.py
@@ -13,7 +13,15 @@ urlpatterns = [
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 
     # /music/album/add
-    url(r'^album/add/$', views.AlbumCreate.as_view(), name='album-add')
+    url(r'^album/add/$', views.AlbumCreate.as_view(), name='album-add'),
+
+    # /music/album/2
+    url (r'^album/(?P<pk>[0-9]+)/$', views.AlbumUpdate.as_view (), name='album-update'),
+
+    # /music/album/2/delete
+    url (r'^album/(?P<pk>[0-9]+)/delete/$', views.AlbumDelete.as_view (), name='album-delete'),
+
+
 
     # /music/<album.id>/favorite
     # url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
